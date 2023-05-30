2a4b91bdde4c1662a1e47e0fc3b3fbb90e919545
João <joaovictor.ramos34@gmail.com>
2023-05-30 11:45:49 -0300

    configurações iniciais

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import  GlobalStyle  from "./styles/global";
+import  theme  from "./styles/theme";
+import { Home } from "./pages/Home";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <Home />
+    </ThemeProvider>
+  </React.StrictMode>
+);
